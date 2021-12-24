da1a818957b49f75535827c5c67e199b3b5ad2a0
viktor <dangerouzua@gmail.com>
2021-12-24 09:27:00 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import os
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
+from sqlalchemy.sql import ClauseElement
+import telebot
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = ''
+app.jinja_env.auto_reload = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+class Group(db.Model):
+    __tablename__ = 'group'
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    uid = db.Column(db.String)
+    name = db.Column(db.String)
+
+
+class Lessons(db.Model):
+    __tablename__ = 'lessons'
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    room = db.Column(db.String)
+    subject = db.Column(db.String)
+    teacher = db.Column(db.String)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    group = db.Column(db.String)
+    order = db.Column(db.String)
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    nickname = db.Column(db.String)
+    group = db.ForeignKey(Group)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://vpu7shedule.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+def get_or_create(session, model, defaults=None, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).one_or_none()
+    if instance:
+        return instance, False
+    else:
+        params = {k: v for k, v in kwargs.items() if not isinstance(v, ClauseElement)}
+        params.update(defaults or {})
+        instance = model(**params)
+        try:
+            session.add(instance)
+            session.commit()
+        except Exception:  # The actual exception depends on the specific database so we catch all exceptions. This is similar to the official documentation: https://docs.sqlalchemy.org/en/latest/orm/session_transaction.html
+            session.rollback()
+            instance = session.query(model).filter_by(**kwargs).one()
+            return instance, False
+        else:
+            return instance, True
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
