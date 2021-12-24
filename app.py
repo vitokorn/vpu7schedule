5f8f90431f3981f66e36bb9907b725a0c21ddb3d
viktor <dangerouzua@gmail.com>
2021-12-24 18:01:10 +0200

    changes in sync

--- app.py
+++ app.py
@@ -482,6 +482,9 @@ def sync():
         group, create = get_or_create(db.session, Group, uid=r['_id'], name=r['name'])
 
     dt = datetime.today()
+    if dt.isoweekday() == 7:
+        db.session.query(Lessons).filter(Lessons.date <= dt).delete()
+        db.session.commit()
     mnd = dt - timedelta(days=dt.weekday())
     monday = (dt - timedelta(days=dt.weekday())).strftime("%Y-%m-%d")
     sunday = (mnd + timedelta(days=5)).strftime("%Y-%m-%d")
