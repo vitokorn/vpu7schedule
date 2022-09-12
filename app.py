e39e16aef071da453105c62f36a39f34fb47edc2
dangerouzua@gmail.com <beatvito24>
2022-09-12 23:43:22 +0300

    changes in inline keyboard

--- app.py
+++ app.py
@@ -759,8 +759,9 @@ def main_menu(message):
         item6 = telebot.types.KeyboardButton("Змінити групу")
         item7 = telebot.types.KeyboardButton("Моя група")
         item8 = telebot.types.KeyboardButton("Як користуватися ботом")
+        item9 = telebot.types.KeyboardButton("Змiнити час отримання розкладу")
         main_text = 'Головне меню'
-        markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
+        markup.add(item1, item2, item3, item4, item5, item6, item7, item8,item9)
         bot.send_message(chat_id=message.chat.id, text='Привiт, ' + message.from_user.first_name, reply_markup=markup)
     elif message.from_user.language_code == "ru":
         item1 = telebot.types.KeyboardButton("Расписание на сегодня")
@@ -771,8 +772,9 @@ def main_menu(message):
         item6 = telebot.types.KeyboardButton("Сменить группу")
         item7 = telebot.types.KeyboardButton("Моя группа")
         item8 = telebot.types.KeyboardButton("Как пользоваться ботом")
+        item9 = telebot.types.KeyboardButton("Изменить время получения расписания")
         main_text = 'Главное меню'
-        markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
+        markup.add(item1, item2, item3, item4, item5, item6, item7, item8,item9)
         bot.send_message(chat_id=message.chat.id, text='Привет, ' + message.from_user.first_name, reply_markup=markup)
     else:
         item1 = telebot.types.KeyboardButton("Schedule for today")
@@ -783,8 +785,9 @@ def main_menu(message):
         item6 = telebot.types.KeyboardButton("Change group")
         item7 = telebot.types.KeyboardButton("My group")
         item8 = telebot.types.KeyboardButton("How to use a bot")
+        item9 = telebot.types.KeyboardButton("Change schedule notification")
         main_text = 'Main menu'
-        markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
+        markup.add(item1, item2, item3, item4, item5, item6, item7, item8,item9)
     bot.send_message(chat_id=message.chat.id, text=f'{main_text}', reply_markup=markup)
     return
 
