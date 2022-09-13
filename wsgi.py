82ddb334ca209b32605c706a5a8154721529cc14
dangerouzua@gmail.com <beatvito24>
2022-09-13 15:28:08 +0300

    test

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app
 
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
